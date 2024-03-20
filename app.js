9a628e1f4d2914e59c98f447d5a3a81f29cb3937
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2024-03-20 16:48:31 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
