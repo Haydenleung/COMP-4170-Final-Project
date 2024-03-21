3997b7ff753c7610528d0cf75ff47ef03adfb253
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2024-03-21 16:52:27 -0700

    add register page

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+
+app.get("/main", (req, res) => {
+    res.render("main.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
