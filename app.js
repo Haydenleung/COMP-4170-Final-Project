ab92b50f75037d7afe25053088f19d8a811e5637
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2024-03-24 19:10:05 -0700

    add database to login

--- app.js
+++ app.js
@@ -1,26 +1,93 @@
 import express from "express";
-import pg from "pg";
+import pg from "pg"
 
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "sevensips",
+  password: "FVFusion2006!", 
+  port: 5432,
+});
+db.connect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs"); 
+  });
+app.get("/main", (req, res) => {
+  res.render("main.ejs");
 });
 
 app.get("/register", (req, res) => {
-    res.render("register.ejs");
+  res.render("register.ejs");
 });
 
-app.get("/main", (req, res) => {
-    res.render("main.ejs");
+app.get("/getData", async (req, res) => {
+  try {
+  
+    const result = await db.query("SELECT * FROM users");
+    res.json(result.rows); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedPassword = user.password;
+
+      if (password === storedPassword) {
+    
+        res.redirect('/main');
+      } else {
+        res.send("Incorrect Password");
+      }
+    } else {
+      res.send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
